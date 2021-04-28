2b80c39a22291b3f5c607b46183ca96b4583adc5
RayDarar <dfqgth400@gmail.com>
2021-04-29 02:56:01 +0600

    work(player.animations): run & idle

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -5,12 +5,19 @@ public class PlayerMovement : MonoBehaviour
   public float moveSpeed = 5f;
 
   public new Rigidbody2D rigidbody;
+  public Animator animator;
 
   private Vector2 movement;
   public void Update()
   {
     movement.x = Input.GetAxisRaw("Horizontal");
     movement.y = Input.GetAxisRaw("Vertical");
+
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Speed", movement.sqrMagnitude);
+    if (movement.x != 0) {
+      animator.SetBool("IsRight", movement.x >= 0);
+    }
   }
 
   public void FixedUpdate()
