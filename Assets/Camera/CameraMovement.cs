11f5fde8550c73c35f3021b560be518c537452db
RayDarar <dfqgth400@gmail.com>
2021-04-30 15:10:09 +0600

    work(camera): smooth movement

---
+++ Assets/Camera/CameraMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+  public Transform target;
+
+  public float cameraSpeed = 0.125f;
+  public Vector3 offset;
+
+  public void LateUpdate()
+  {
+    transform.position = Vector3.Lerp(transform.position, target.position + offset, cameraSpeed);
+  }
+}
